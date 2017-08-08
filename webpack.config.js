b71b2604de33ff21ab4b06111fe30f0392c6951b
Marco Riolo <mriolo.it@gmail.com>
2017-08-08 18:41:55 +0200

    Starting project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
     module: {
         loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
             {test: /\.scss$/, loader: ExtracttextPlugin.extract('css!scss!sass')}
         ]
     },
